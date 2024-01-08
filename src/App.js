a52fe01765d1504e1693db9963f10270fbf9603d
Avinash Jha <avinash.jha@easebuzz.in>
2024-01-08 13:35:06 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="top-text">
+        <p>Hi, users</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
